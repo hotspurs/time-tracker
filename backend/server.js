e65f74a5e2918c8f309466ee871379200d426772
Vlad Dubov <dubov@betaagency.ru>
2015-12-08 00:07:31 +0300

    Добавил локальную стратегию для авторизации

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ var express = require('express'),
     session = require('express-session'),
     SessionStore = require('express-mysql-session'),
     app = module.exports = express(),
+    passport = require('passport'),
     path = require('path'),
     db = require('./libs/db.js'),
     cookieParser = require('cookie-parser'),
@@ -14,7 +15,7 @@ var express = require('express'),
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME
-    });
+    }), LocalStrategy = require('passport-local').Strategy;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -34,6 +35,49 @@ app.use(session({
     }
 }));
 
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  
+  app.locals.knex('user').where('id', id).then(function(user){
+    console.log('=>>>>>', user)
+    done(null, user);
+  });
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+  },
+  function(username, password, done) {
+
+    console.log('email =>>>', username);
+    console.log('password =>>>', password);
+
+    app.locals.knex('user').where('email', username).then(function(user){
+
+      if (!user) {
+        console.log('LOG IN 1', user);
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (password !== user[0].password ) {
+        console.log('LOG IN 2', user);
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+
+
+
+      return done(null, user[0]);
+    });
+  }
+));
+
+
 if (process.env.NODE_ENV === 'development') {
   var webpackMiddleware = require('./libs/webpack-middleware.js');
   app.use(webpackMiddleware.webpackDevMiddleware);
@@ -45,6 +89,17 @@ if (process.env.NODE_ENV === 'development') {
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
+
+app.get('/login', function(req, res) {
+  res.sendFile(path.join(__dirname, '../frontend/login.html'));
+});
+
+app.post('/login',
+  passport.authenticate('local', { successRedirect: '/',
+                                   failureRedirect: '/login',
+                                   failureFlash: false })
+);
+
 
 app.use('/api/projects', require('./api/projects'));
 
