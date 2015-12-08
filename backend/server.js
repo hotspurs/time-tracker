5dc9cb67f754bd2da018e51b56b4713ba65f5401
Vlad Dubov <dubov@betaagency.ru>
2015-12-08 23:57:13 +0300

    Добавил vk стратегию

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,16 @@ var express = require('express'),
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME
-    }), LocalStrategy = require('passport-local').Strategy;
+    }), LocalStrategy = require('passport-local').Strategy,
+    VKontakteStrategy = require('passport-vkontakte').Strategy;
+
+if (process.env.NODE_ENV === 'development') {
+  var webpackMiddleware = require('./libs/webpack-middleware.js');
+  app.use(webpackMiddleware.webpackDevMiddleware);
+  app.use(webpackMiddleware.webpackHotMiddleware);
+} else {
+  app.use(express.static(path.join(__dirname, '../build')))
+}
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -50,6 +59,27 @@ passport.deserializeUser(function(id, done) {
 app.use(passport.initialize());
 app.use(passport.session());
 
+passport.use(new VKontakteStrategy({
+    clientID:     5180715, // VK.com docs call it 'API ID'
+    clientSecret: 'cd8dv8FcTWPtpt0m9hf0',
+    callbackURL:  "http://local.dubov-developer.com:3000/auth/vkontakte/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    app.locals.knex('user').insert({ email: profile.id, password: 12345 }).then(function(id){
+
+      var user = {}
+      user.id = id[0];
+
+      done(null, user);
+    });
+  }
+));
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login');
+}
+
 passport.use(new LocalStrategy({
     usernameField: 'email',
     passwordField: 'password'
@@ -62,31 +92,18 @@ passport.use(new LocalStrategy({
     app.locals.knex('user').where('email', username).then(function(user){
 
       if (!user) {
-        console.log('LOG IN 1', user);
         return done(null, false, { message: 'Incorrect username.' });
       }
       if (password !== user[0].password ) {
-        console.log('LOG IN 2', user);
         return done(null, false, { message: 'Incorrect password.' });
       }
-
-
 
       return done(null, user[0]);
     });
   }
 ));
 
-
-if (process.env.NODE_ENV === 'development') {
-  var webpackMiddleware = require('./libs/webpack-middleware.js');
-  app.use(webpackMiddleware.webpackDevMiddleware);
-  app.use(webpackMiddleware.webpackHotMiddleware);
-} else {
-  app.use(express.static(path.join(__dirname, '../build')))
-}
-
-app.get('/', function(req, res) {
+app.get('/', ensureAuthenticated, function(req, res) {
   res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
@@ -94,14 +111,27 @@ app.get('/login', function(req, res) {
   res.sendFile(path.join(__dirname, '../frontend/login.html'));
 });
 
-app.post('/login',
-  passport.authenticate('local', { successRedirect: '/',
-                                   failureRedirect: '/login',
-                                   failureFlash: false })
-);
+app.post('/login', passport.authenticate('local', { successRedirect: '/',
+                                                    failureRedirect: '/login',
+                                                    failureFlash: false 
+                                                  }));
 
+app.get('/auth/vkontakte', passport.authenticate('vkontakte'), function(req, res){
 
-app.use('/api/projects', require('./api/projects'));
+});
+
+app.get('/auth/vkontakte/callback', passport.authenticate('vkontakte', { 
+  failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+});
+
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/');
+});
+
+app.use('/api/projects', ensureAuthenticated, require('./api/projects'));
 
 var server = app.listen(process.env.PORT, process.env.HOST, function() {
   var host = server.address().address,
